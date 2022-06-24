9ce96ffdac251d95726c017cd051b0a91a83b020
Shehryar Awan <shehryartariq82@hotmail.com>
2022-06-24 09:48:25 -0400

    Basic Setup completed
    The base api and config setup has been completed.

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,15 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.port;
+
+//console.log(process.env.NODE_ENV);
 
 //Server Start
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
