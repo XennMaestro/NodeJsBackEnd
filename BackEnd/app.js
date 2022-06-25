05e51384911d140f6b026d4d824c2d5f1f31de80
Shehryar Awan <shehryartariq82@hotmail.com>
2022-06-25 11:24:34 -0400

    Fixed Docker issues
    Fixed Docker issues

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const app = express();
+
 const userRouter = require('./routes/userRoutes');
 
 //Global MiddleWare
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,12 +4,8 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const port = process.env.port;
-
-//console.log(process.env.NODE_ENV);
-
 //Server Start
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+app.listen(3000, () => {
+    console.log(`App running on port ${3000}...`);
 });
 
