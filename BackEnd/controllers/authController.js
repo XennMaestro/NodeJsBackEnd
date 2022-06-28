4c166dce8a81daaf9c774b924109066ffd198ca1
Shehryar Awan <shehryartariq82@hotmail.com>
2022-06-28 12:49:09 -0400

    LogIn and SignUp module completed
    I have completed the functionality surrounding basic user signup and basic user login functionality. This is impletemented using the JSON Web Tokens approach.

--- BackEnd/controllers/authController.js
+++ BackEnd/controllers/authController.js
@@ -1,15 +1,61 @@
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/userModel");
 
 const catchAsync = require("../Utils/catchAsync");
 
+const AppError = require("../Utils/appError");
+
+//Create a JSON Web Token JWT
+const signToken = id => jwt.sign({ id: id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+
 //SignUp a new user into the application
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+        email: req.body.email,
+        age: req.body.age
+    });
+
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
     });
+});
+
+//LogIn a new user into the application
+exports.login = catchAsync(async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //1) Check if email and password exist
+    if(!email || !password){
+      return next(new AppError('Please provide email and password', 400));
+    }
+
+    //2) Check if user exists && password is correct
+    const user = await User.findOne({email: email}).select('+password');
+
+    if(!user || !(await user.correctPassword(password, user.password))){
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    //3) If everything is ok, send token to client
+    const token = signToken(user._id);
+
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+
 });
--- BackEnd/models/userModel.js
+++ BackEnd/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -13,12 +14,20 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Missing user password'],
-        minLength: 8
+        minLength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
         required: [true, 'Missing user password'],
-        minLength: 8
+        minLength: 8,
+        validate: {
+            //This only works on CREATE and SAVE!!!
+            validator: function(el) {
+                return el === this.password
+            },
+            message: 'Passwords are not the same!'
+        }
     },
     email: {
         type: String,
@@ -32,6 +41,26 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Missing user age']
     },
 });
+
+userSchema.pre('save', async function(next) {
+    //Only run this function if passwords was acutally modified
+    if(!this.isModified('password')) return next();
+
+    //Hash password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Clear matching password, since it is no longer needed
+    this.passwordConfirm = undefined;
+
+    next();
+});
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
