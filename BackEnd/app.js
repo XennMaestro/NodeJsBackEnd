51fbff337122852495b14b1921921c461e3bfa08
Shehryar Awan <shehryartariq82@hotmail.com>
2022-06-30 10:58:27 -0400

    Authorizatoin and Authenticaton implemented
    I have completed the Authorization and Authentication segments.

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,28 +2,72 @@ const express = require('express');
 
 const app = express();
 
-const AppError = require('./Utils/appError');
+const rateLimit = require('express-rate-limit');
+
+const helmet = require('helmet');
+
+const morgan = require('morgan');
+
+const mongoSanitize = require('express-mongo-sanitize');
+
+const xss = require('xss-clean');
+
+const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
 
 const globalErrorHandler = require('./controllers/errorController');
 
 const userRouter = require('./routes/userRoutes');
 
 
-//Global MiddleWare
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+//1)Global MiddleWare
+    //Security HTTP Headers
+    app.use(helmet());
 
-//Routes
-app.use('/api/v1/users', userRouter);
+    app.use(express.json());
+    app.use(express.urlencoded({
+        extended: true
+    }));
 
-//Undefined Route Handler
-app.all('*', (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-});
+    //Development loggin
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    }
 
-//Error handling middleware
-app.use(globalErrorHandler);
+    //Limit request from same API
+    const limiter = rateLimit({
+        max: 100,
+        windowMs: 60 * 60 * 1000,
+        message: 'Too many request from this IP. Please try again in an hour.'
+    });
 
-module.exports = app;
+    app.use('/api',limiter);
+
+    //Body Parser, reading data from body into req.body
+    app.use(express.json({
+        limit: '10kb'
+    }));
+
+    //Data sanitization against NoSQL query injection
+    app.use(mongoSanitize());
+
+    //Data sanitization against XSS attacks
+    app.use(xss());
+
+    //Prevent parameter pollution
+    app.use(hpp({
+        whitelist: ['duration']
+    }));
+//2)Routes
+    app.use('/api/v1/users', userRouter);
+
+    //Undefined Route Handler
+    app.all('*', (req, res, next) => {
+        next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+    });
+
+    //Error handling middleware
+    app.use(globalErrorHandler);
+
+    module.exports = app;
--- BackEnd/routes/userRoutes.js
+++ BackEnd/routes/userRoutes.js
@@ -6,6 +6,14 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
     .route('/')
@@ -15,7 +23,6 @@ router
     router
     .route('/:id')
     .get(authController.protect, userController.getUser)
-    .delete(authController.protect, authController.restrictTo('admin'), userController.deleteUser)
     .patch(authController.protect, userController.updateUser);
 
 module.exports = router;    
